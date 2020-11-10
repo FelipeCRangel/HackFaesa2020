fccb6363371314222880f8998be4cbc867d61523
Felipe  Rangel <carvalhofeliperangel@gmail.com>
2020-11-10 14:55:50 -0300

    Arquivo index.js criado

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './reset.css'
+// 
+// import Auth from '../src/screens/Auth'
+// import Home from '../src/screens/Home'
+// import Dashboard from '../src/screens/Produtores/Dashboard'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     {/* <Auth /> */}
+//     <Home /> 
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// reportWebVitals();
